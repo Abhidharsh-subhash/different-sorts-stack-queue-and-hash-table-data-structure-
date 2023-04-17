c9bdf2411b74ef707f31fc3c78a5512bf0987415
abhidharshss <abhidharsh1999@gmail.com>
2023-04-17 10:27:35 +0530

    hash

--- hashtable/separatechaining.py
+++ hashtable/separatechaining.py
@@ -1,23 +1,46 @@
 class hashtable:
     def __init__(self):
-        self.max=100
+        self.max=10
         self.arr=[[] for i in range(self.max)]
     
     def get_hash(self,key):
         s=0
         for i in key:
             s+=ord(i)
-        return s%100
+        return s%10
     
     def __setitem__(self,key,value):
         s=self.get_hash(key)
-        self.arr[s]=value
+        found=False
+        #here we are using tuple so it is immutable
+        for index,element in enumerate(self.arr[s]):
+            if element[0] == key and len(element) ==2:
+                self.arr[s][index] = (key,value)
+                found=True
+                break
+        if not found:
+            self.arr[s].append((key,value))
 
     def __getitem__(self,key):
         s=self.get_hash(key)
-        return self.arr[s]
+        for element in self.arr[s]:
+            if element[0] == key:
+                return element[1]
     
     def __delitem__(self,key):
         s=self.get_hash(key)
-        self.arr[s]=None
+        for index,element in enumerate(self.arr[s]):
+            if element[0] == key:
+                del self.arr[s][index]
+
+
+h=hashtable()
+h['march 6']=290
+h['march 6']=78
+h['march 8']=67
+h['march 9']=4
+h['march 17']=459
+del h['march 17']
+print(h.arr)
+
 
