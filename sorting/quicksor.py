2caf864acfd516b6473ab6c8053c307944c74821
abhidharshss <abhidharsh1999@gmail.com>
2023-04-19 19:55:09 +0530

    quick

--- sorting/quicksor.py
+++ sorting/quicksor.py
@@ -13,17 +13,11 @@ def partition(arr):
         if start < end:
             arr[start],arr[end]=arr[end],arr[start]
     arr[pivort_index],arr[end]=arr[end],arr[pivort_index]
-    return end
 
 def quicksort(array):
-    if len(array) <=1:
-        return array
-    pivot_index=partition(array)
-    left=array[:pivot_index-1]
-    right=array[pivot_index+1:]
-    return left+[array[pivot_index]]+right
+    partition(array)
 
 arr1=[11,9,29,7,2,15,28]
-s=quicksort(arr1)
-print(s)
+quicksort(arr1)
+print(arr1)
 
---
+++ sorting/sample.py
@@ -0,0 +1,30 @@
+def partition(arr):
+    pivot_index = 0
+    pivot = arr[pivot_index]
+
+    start = pivot_index + 1
+    end = len(arr) - 1
+
+    while start <= end:
+        while start <= end and arr[start] <= pivot:
+            start += 1
+        while arr[end] > pivot:
+            end -= 1
+        if start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+    arr[pivot_index], arr[end] = arr[end], arr[pivot_index]
+    return end
+
+def quicksort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot_index = partition(array)
+    left_partition = quicksort(array[:pivot_index])
+    right_partition = quicksort(array[pivot_index + 1:])
+    
+    return left_partition + [array[pivot_index]] + right_partition
+
+arr1 = [11, 9, 29, 7, 2, 15, 28]
+print(quicksort(arr1)) # Output: [2, 7, 9, 11, 15, 28, 29]
+
